5db26d9c8ab3606d90bbe18e014cbc2bffeea7e8
Mashitha <mashitharazanarv@gmail.com>
2026-02-21 20:21:18 +0530

    Added datatypes related files and KT items.

---
+++ datatypes.py
@@ -0,0 +1,57 @@
+#string
+#message="Hey Bro, wassup"
+#name='Python'
+
+#poem="""
+#Roses are red,
+#Violets are blue,
+#Python is awesome,
+#Well so are you!!
+#"""
+
+#string operations
+#first_name="Mashitha"
+#last_name="Razana"
+
+#concatenate
+#fullname=first_name +" "+ last_name
+#print(fullname.upper())
+
+#repeatative characters
+#line="*" * len(fullname)
+#print(line)
+
+#print(fullname[3])
+
+#Integer
+#a=10
+#b=10
+
+#print("Addition: ",a+b)
+#print("Subtraction: ",a-b)
+#print("Multiplication: ",a*b)
+#print("Division: ",a/b)
+#print("Integer division: ",a//b)
+#print("Modulus: ",a%b)
+#print("Exponent: ",a**b)
+
+#floats
+price=20.99
+PI=3.14285
+temp=99.8
+
+total=price*1.5
+print(total)
+
+print(0.1+0.345)
+print(round(0.1+0.345,5))
+
+
+#Booleans
+
+is_raining=False
+is_holiday=False
+age=10
+
+is_adult=age >=18
+#is_adult=False
