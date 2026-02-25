54b9c7e8e5c45dccefb49ab93aba23e3307a5e9a
Mashitha <mashitharazanarv@gmail.com>
2026-02-25 13:57:43 +0530

    Updated

---
+++ csv_file_ops.py
@@ -0,0 +1,28 @@
+# import os
+import csv
+
+# with open("db.csv","r") as file:
+#    for line in file:
+#        data = line.strip().split(",")
+#        print("Dear" ,data[1],", Your user ID is ",data[0])
+#        response=os.system("ping -c 1 "+ data[3])
+
+# with open("students.csv","w") as file:
+#    file.write("name,age,grade\n")
+#    file.write("Mashi,21,A\n")
+#    file.write("Farheen,25,B\n")
+
+# students = [
+#    ["name","age","grade"],
+#    ["Mashi",21,"A"],
+#    ["Farheen",25,"B"]
+# ]
+
+# with open("new_students.csv","w",newline='') as file:
+#    writer = csv.writer(file)
+#    writer.writerows(students)
+
+# with open("new_students.csv","r") as file:
+#    reader = csv.reader(file)
+#    for row in reader:
+#        print(row)
--- file.py
+++ file.py
@@ -12,7 +12,19 @@
 #    for line in lines:
 #        print(line.strip())
     
-with open("output.txt","w") as file:
-    file.write("Hello World 2 \n")
-    file.write("This is a new file\n")
-    
+# with open("output.txt","w") as file:
+#    file.write("Hello World 2 \n")
+#    file.write("This is a new file\n")
+
+
+try:
+    with open("students_new.csv","r") as file:
+        content=file.read()
+except FileNotFoundError:
+    print("File not found") 
+except PermissionError:
+    print("You don't have permission to access this file")
+except SyntaxError:
+    print("There is error in your syntax")
+except Exception as e:
+    print(f"An error occurred: {e}")
